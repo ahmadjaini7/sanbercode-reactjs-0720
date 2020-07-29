e1a43ebfd9687bc3c1fef0841ed8be7139346ae7
Ahmad Jaini <ahmad.jaini@students.uin-suska.ac.id>
2020-07-29 07:19:17 +0700

    menambahkan tugas ke 8

---
+++ Pekan 2/Tugas Hari Ke 7/index2.js
@@ -0,0 +1,20 @@
+var readBooksPromise = require('./promise.js')
+
+var books = [{
+        name: 'LOTR',
+        timeSpent: 3000
+    },
+    {
+        name: 'Fidas',
+        timeSpent: 2000
+    },
+    {
+        name: 'Kalkulus',
+        timeSpent: 4000
+    }
+]
+
+// Lanjutkan code untuk menjalankan function readBooksPromise 
+
+// Bang saya gak paham materi dan tugasnya berbeda kali. Jadi saya kerjakan sampai sini aja.
+// Kalau tugasnya seperti dicontoh buat callback dan promise dari awal. mungkin lebih mudah dipahami untuk pemula bang
